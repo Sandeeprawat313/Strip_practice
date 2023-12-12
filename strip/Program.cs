38e32618059bb6727b5c134b11ed44f6a6530cce
Sandeep Rawat <132456591+sandeep-narayana@users.noreply.github.com>
2023-12-12 18:19:10 +0530

    added stripe to the project

--- strip/Program.cs
+++ strip/Program.cs
@@ -1,3 +1,5 @@
+using Stripe;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -15,6 +17,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+StripeConfiguration.ApiKey = builder.Configuration.GetSection("Stripe:SecretKey").Get<string>();
 
 app.UseAuthorization();
 
